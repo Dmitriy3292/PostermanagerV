4bca16d6c303ee801ec0444bc7654df928bb237b
Dmitriy3292 <mr.diy@yandex.ru>
2022-03-10 18:20:45 +0400

    added test

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -5,12 +5,12 @@ import lombok.NoArgsConstructor;
 import ru.netology.domain.Film;
 
 @AllArgsConstructor
-@NoArgsConstructor
+
 
 public class AfishaManager {
 
     private Film[] films = new Film[0];
-    private int CountMovies = 10;
+    private int countMovies = 10;
 
     public AfishaManager() {
     }
@@ -18,6 +18,38 @@ public class AfishaManager {
     public AfishaManager(Film[] films) {
         this.films = films;
     }
+
+    public AfishaManager(int Film) {
+    }
+
+
+    public void add(Film film) {
+        int length = films.length + 1;
+        Film[] tmp = new Film[length];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = film;
+        films = tmp;
+    }
+
+    public Film[] getLastFilms() {
+        int resultlenght;
+        if (films.length > countMovies) {
+            resultlenght = countMovies;
+        } else {
+            resultlenght = films.length;
+        }
+        Film[] result = new Film[resultlenght];
+        for (int i = 0; i < resultlenght; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
+    }
 }
+
+
 
 
